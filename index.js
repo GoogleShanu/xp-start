c711655cdb693c0431e5634ae541e26eeb702fbe
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2019-01-14 20:49:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const activities_list = [
  //     client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
  //   }, 20000); // Runs this every 10 seconds.
 	
-   client.user.setGame('Chill Vibes Subscribe Now', 'https://www.youtube.com/c/WolfBoyShanNightcoreknight');
+   client.user.setGame('Chill Vibes | ✔️Subscribe Now', 'https://www.youtube.com/c/WolfBoyShanNightcoreknight');
 });
 
 client.on("message", (message) => {
