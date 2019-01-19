1a0a908728d48d45672ba31fb899761b1d58ab26
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2019-01-19 13:47:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ client.on("message", (message) => {
             // use the message's channel (TextChannel) to send a new message
           message.channel.send("boosting XP!!")
            .catch(console.error); // add error handling here
-       }, 2 * 1100); 
+       }, 1 * 700); 
    }
 		
     
